a99876b008b80ad2ee5c240155ee10fbb7427398
Conner Andrews <and16054@byui.edu>
2020-09-26 22:40:29 -0600

    uploaded index page and assignments page.

--- web/index.php
+++ web/index.php
@@ -1,5 +1,310 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <title>Conner Andrews' Homepage</title>
+</head>
+<body>
+  <main>
+  
+    <div class=grid-container>
+      <div class="header grid-child">
+        <div class="header-grid-container">
+            <span class="logo"><img src="LADodgersLogo.jpg" alt=DodgerLogo width=100px id=logo></span>
+            <span class="school"><u>Conner Andrews' Homepage</u></span>
+        </div>
+      </div>
+      <div class="grid-child sidebar">
+                <div class=secondary-grid-container>
+                    <div class="secondary-grid-child homepage">
+                        <a href="assignments.html">Weekly Assignments</a>
+                    </div>
+                    <div class="secondary-grid-child record">
+                        <a href="#record">Current Record</a>
+                    </div>
+                </div>
+      </div>
+      <div class="grid-child main">
+                <div class="academics-grid">
+                    <h1 class="title">LA Dodgers</h1>
+                    <span>
+                    I absolutely love baseball and despite living in Idaho, I am a huge LA Dodger fan. This page includes a few stats and facts about the Dodgers from this year.  
+                    </span>  
+                    <h2>Team Roster</h2>
+                    <table>
+                      <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Number </th>
+                        <th>Position</th>
+                        <th>AVG</th>
+                        <th>RBI</th>
+                        <th>HR</th>
+                      <tr>
+                        <th>Pedro</th>
+                        <th>Baez</th>
+                        <th>52</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+                      
+                      <tr>
+                        <th>Austin</th>
+                        <th>Barnes</th>
+                        <th>15</th>
+                        <th>C</th>
+                        <th>.250</th>
+                        <th>9</th>
+                        <th>1</th>
 
-  phpinfo();
+                      <tr>
+                        <th>Cody</th>
+                        <th>Bellinger</th>
+                        <th>35</th>
+                        <th>RF</th>
+                        <th>RF</th>
+                        <th>29</th>
+                        <th>12</th>
 
-?>
+                      <tr>
+                        <th>Mookie</th>
+                        <th>Betts</th>
+                        <th>50</th>
+                        <th>RF</th>
+                        <th>.292</th>
+                        <th>39</th>
+                        <th>16</th>
+
+                      <tr>
+                        <th>Walher</th>
+                        <th>Buehler</th>
+                        <th>21</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Dylan</th>
+                        <th>Floro</th>
+                        <th>51</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Tony</th>
+                        <th>Gonsolin</th>
+                        <th>46</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Victor</th>
+                        <th>Gonzalez</th>
+                        <th>81</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Brusdar</th>
+                        <th>Graterol</th>
+                        <th>48</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Enrique</th>
+                        <th>Hernandez</th>
+                        <th>14</th>
+                        <th>2B</th>
+                        <th>.227</th>
+                        <th>20</th>
+                        <th>5</th>
+
+                      <tr>
+                        <th>Kenley</th>
+                        <th>Jansen</th>
+                        <th>74</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Joe</th>
+                        <th>Kelly</th>
+                        <th>17</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Clayton</th>
+                        <th>Kershaw</th>
+                        <th>22</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Adam</th>
+                        <th>Kolarek</th>
+                        <th>56</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+                      
+                      <tr>
+                        <th>Gavin</th>
+                        <th>Lux</th>
+                        <th>9</th>
+                        <th>2B</th>
+                        <th>.180</th>
+                        <th>8</th>
+                        <th>3</th>
+
+                      <tr>
+                        <th>Dustin</th>
+                        <th>May</th>
+                        <th>85</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Jake</th>
+                        <th>McGee</th>
+                        <th>41</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Max</th>
+                        <th>Muncy</th>
+                        <th>13</th>
+                        <th>1B</th>
+                        <th>.197</th>
+                        <th>27</th>
+                        <th>12</th>
+
+                      <tr>
+                        <th>Joc</th>
+                        <th>Pederson</th>
+                        <th>31</th>
+                        <th>LF</th>
+                        <th>.183</th>
+                        <th>13</th>
+                        <th>6</th>
+
+                      <tr>
+                        <th>AJ</th>
+                        <th>Pollock</th>
+                        <th>11</th>
+                        <th>CF</th>
+                        <th>.265</th>
+                        <th>31</th>
+                        <th>14</th>
+
+                      <tr>
+                        <th>Edwin</th>
+                        <th>Rios</th>
+                        <th>43</th>
+                        <th>3B</th>
+                        <th>.243</th>
+                        <th>16</th>
+                        <th>7</th>
+
+                      <tr>
+                        <th>Corey</th>
+                        <th>Seager</th>
+                        <th>5</th>
+                        <th>SS</th>
+                        <th>.319</th>
+                        <th>41</th>
+                        <th>15</th> 
+
+                      <tr>
+                        <th>Will</th>
+                        <th>Smith</th>
+                        <th>16</th>
+                        <th>C</th>
+                        <th>.290</th>
+                        <th>23</th>
+                        <th>7</th>
+
+                      <tr>
+                        <th>Chris</th>
+                        <th>Taylor</th>
+                        <th>3</th>
+                        <th>LF</th>
+                        <th>.277</th>
+                        <th>32</th>
+                        <th>8</th>  
+
+                      <tr>
+                        <th>Blake</th>
+                        <th>Treinen</th>
+                        <th>49</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Justin</th>
+                        <th>Turner</th>
+                        <th>10</th>
+                        <th>3B</th>
+                        <th>.310</th>
+                        <th>23</th>
+                        <th>4</th>
+
+                      <tr>
+                        <th>Julio</th>
+                        <th>Urias</th>
+                        <th>7</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+
+                      <tr>
+                        <th>Alex</th>
+                        <th>Wood</th>
+                        <th>57</th>
+                        <th>P</th>
+                        <th>--</th>
+                        <th>--</th>
+                        <th>--</th>
+                  </table>
+                </div>
+      </div>
+    <div class="grid-child footer" id="record">
+      <h2>Current Record</h2>
+      <?php
+        echo "As of " . date("m/d/Y") . " the Dodgers are 41-17.";
+      ?>
+
+    </div>
+    </div>
+  </main>
+  
+</body>
+</html>
